ad19c9bdb1ec305782bdc810d1e4b4c9a32d2c67
Andrii <andrii.kahui.knm.2019@lpnu.ua>
2023-05-09 17:38:25 +0300

    CNN code added

--- EmotionRecognition/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ EmotionRecognition/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace EmotionRecognition {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EmotionRecognition;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EmotionRecognition;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
